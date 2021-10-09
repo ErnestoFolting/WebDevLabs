5325b46a22e8099b983e376cd28a7826bf77ecdc
Danil <danilsmy.edu@gmail.com>
2021-10-10 01:20:59 +0300

    Add external js for form

---
+++ Lab2/src/routes/index.js
@@ -0,0 +1,11 @@
+"use strict"
+
+document.addEventListener('DOMContentLoaded',function(){
+    const form = document.getElementById('form');
+	form.addEventListener('submit',formSend);
+
+	async function formSend(e){
+		e.preventDefault();
+		console.log('check');
+    }
+});
--- Lab2/src/routes/index.svelte
+++ Lab2/src/routes/index.svelte
@@ -4,7 +4,6 @@
 
 <script>
 	import Counter from '$lib/Counter.svelte';
-
 </script>
 
 <svelte:head>
