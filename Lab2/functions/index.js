b683722eafe62b0d5466ebef6c248cfc586bdd71
Danil <danilsmy.edu@gmail.com>
2021-10-15 01:18:00 +0300

    fix formData send

--- Lab2/functions/index.js
+++ Lab2/functions/index.js
@@ -15,28 +15,25 @@ const transporter = nodemailer.createTransport({
 exports.sendmail = functions.https.onRequest((req, res) => {
   functions.logger.info("Hello logs!", {structuredData: true});
   console.log(req.body);
-  console.log(req.body);
-  if (!Object.keys(req.body).length) {
-    return res.status(400).json({code: 400, error: "No message!"});
-  }
+  console.log(Object.keys(req.body).length);
+
   const lines = Object.entries(req.body)
       .map(([key, val]) => `<p><b>${key}: </b>${val}</p>`)
       .join("\n");
-
-  const html = sanitizeHtml(`<h1> Message from form: </h1>${lines}`);
-
+  const myHtml = sanitizeHtml(`<h1> Message from form: </h1>${lines}`);
   const mailOptions = {
     from: "Contact form",
     to: "danilsmy.edu@gmail.com",
     subject: "Hary, nice form!!!",
-    html: html,
+    html: myHtml,
   };
 
   transporter.sendMail(mailOptions, (error) => {
     if (error) {
       console.error("Error sending mail", error.message);
       return res.status(500).json({code: "500", eror: error.message});
+    } else {
+      return res.status(200).json({data: "ok"});
     }
-    return res.status(200).json({data: "ok"});
   });
 });
