e0529c65beb6e37b63d60ce0b168bc9fa68f931f
Danil <danilsmy.edu@gmail.com>
2021-11-26 01:58:34 +0200

    Add an easy data from tables show

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,26 +1,93 @@
-<script context="module">
-	export const prerender = true;
+<script>
+
+async function fetchGraphQL(operationsDoc, operationName, variables) {
+  const result = await fetch(
+    "https://kpilab3.herokuapp.com/v1/graphql",
+    {
+      method: "POST",
+      body: JSON.stringify({
+        query: operationsDoc,
+        variables: variables,
+        operationName: operationName
+      })
+    }
+  );
+
+  return await result.json();
+}
+
+const operationsDoc = `
+  query MyQuery {
+    notes(order_by: {id: desc_nulls_first}) {
+      author
+      date
+      text
+      id
+    }
+  }
+`;
+
+function fetchMyQuery() {
+  return fetchGraphQL(
+    operationsDoc,
+    "MyQuery",
+    {}
+  );
+}
+
+async function startFetchMyQuery() {
+  const { errors, data } = await fetchMyQuery();
+
+  if (errors) {
+    // handle those errors like a pro
+    console.error(errors);
+  }
+
+  // do something great with this precious data
+  console.log(data);
+}
+
+startFetchMyQuery();
+
 </script>
 
 <svelte:head>
 	<title>To-Dos</title>
 </svelte:head>
-
 <section>
-	<h1>Test Lab3</h1>
-	<script>
-	</script>
+
+	<h1>To-Dos list:</h1>
+
+	<div class = "notes">
+	{#await fetchMyQuery()}
+		<p>...waiting</p>
+	{:then data}
+		<p>The number is {data.data.notes.length}</p>
+		{#each data.data.notes as {author,date,text}}
+		<p>
+			{author} - {date} - {text}
+		</p>
+	{/each}
+	{:catch error}
+		<p style="color: red">{error.message}</p>
+	{/await}
+	
+	</div>
+
 </section>
 
 <style>
-	section {
+	section{
 		display: flex;
 		flex-direction: column;
-		justify-content: center;
-		align-items: center;
+		justify-content:center;
+		align-items:center;
 		flex: 1;
 	}
-
+	.notes{
+		background-color: rgba(0, 255, 255, 0.247);
+		border-radius: 15px;
+	}
 	h1 {
 		width: 100%;
 	}
