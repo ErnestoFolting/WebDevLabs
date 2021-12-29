07409ecb34328e12e62da1f3b2b5770638c91cda
Danil <danilsmy.edu@gmail.com>
2021-12-29 04:05:06 +0200

    Add frontend form with svelte sugar

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -2,153 +2,82 @@
 	export const prerender = true;
 </script>
 
+<script>
+	import { Circle3 } from 'svelte-loading-spinners';
+	let textInError = '';
+	let showSpinner = false;
+	let showSuccess = false;
+	let showError = false;
+	let disableButton = false;
+	let form = {
+    reset: () => {},
+  };
+  let mainFormHandler = async e => {
+	  showSuccess = false;
+	  showError = false;
+	  disableButton = true;
+	  showSpinner = true;
+	  let formData = Object.fromEntries(
+		  Array.from(form.elements)
+		  	.filter(element => element.tagName != 'BUTTON')
+			.map(el => [el.name,el.value])
+	  );
+	  formData['referrer'] = document.referrer;
+	  try{
+		let response = await fetch('/api/sendmail', {
+        	method: 'POST',
+        	headers: {
+          	'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(formData),
+      });
+	  if(!response.ok){
+		  throw response;
+	  }
+	  form.reset();
+	  showSuccess = true;
+	  return response;
+  	} catch (e) {
+		if (e.status >= 500) {
+		textInError = 'The error on server';
+      } else if (e.status === 400) {
+        textInError = 'Null message';
+      } else if (e.status === 429) {
+        textInError = 'You sent the form too many times';
+      }
+	  showError = true;
+	  }finally{
+		  showSpinner = false;
+		  disableButton = false;
+	  }
+  }
+</script>
+
 <svelte:head>
 	<title>Sender</title>
 </svelte:head>
 
+
 <section>
-	<div id="responseGood">
-		<h4>Your form was successfuly sent!</h4>
-	</div>
-	<div id="responseBad">
-		<h4>Unfortunately, your form was not sent</h4>
-	</div>
-	<div class="form">
-		<form id="form" class="form__body">
-			<h1 class="form__title">Form sender to mail</h1>
-			<div class="form__item">
-				<label for="formName" class="form__label">Ім'я*</label>
-				<input id="formName" type="text" name="name" class="form__input _req" />
-			</div>
-			<div class="form__item">
-				<label for="formEmail" class="form__label">Пошта*</label>
-				<input id="formEmail" type="text" name="email" class="form__input _req _email" />
-			</div>
-			<div class="form__item">
-				<label for="formMessage" class="form__label">Повідомлення*</label>
-				<textarea name="message" id="formMessage" class="form__input _req" cols="30" rows="6" />
-			</div>
-			<div class="form__item">
-				<div class="form__label">Стать*</div>
-				<select name="sex" class="select _req">
-					<option value="чоловік" selected>Чоловіча</option>
-					<option value="жінка">Жіноча</option>
-				</select>
-				<br />
-				<button type="submit" class="form__button">Відправити</button>
-			</div>
-		</form>
-		<script>
-			'use strict';
-			const debounce = (fn, delay) => {
-				let timeOut;
-				return function (...args) {
-					if (timeOut) {
-						clearTimeout(timeOut);
-					}
-					timeOut = setTimeout(() => {
-						fn(...args);
-					}, delay);
-				};
-			};
-			document.addEventListener('DOMContentLoaded', function () {
-				const form = document.getElementById('form');
-				const responseGood = document.getElementById('responseGood');
-				const responseBad = document.getElementById('responseBad');
-				form.addEventListener('submit', preSend);
-				form.addEventListener('submit', debounce(formSend, 1000));
-				async function preSend(e) {
-					form.classList.add('_sending');
-					e.preventDefault();
-				}
-				async function formSend() {
-					responseBad.classList.remove('_sending');
-					responseGood.classList.remove('_sending');
-					const referrerValue = document.referrer;
-					let formData = {};
-					Array.from(form.elements)
-						.filter((el) => el.tagName !== 'BUTTON')
-						.forEach((element) => {
-							formData[element.name] = element.value;
-						});
-					formData['referrer'] = referrerValue;
-
-					console.log(formData);
-					let error = formValidate();
-					if (!error) {
-						let response = await fetch('/api/sendmail', {
-							method: 'POST',
-							headers: {
-								'Content-Type': 'application/json;charset=utf-8'
-							},
-							body: JSON.stringify(formData)
-						});
-						console.log(response.status);
-						if (response.ok) {
-							responseGood.classList.add('_sending');
-							responseBad.classList.remove('_sending');
-							form.reset();
-							form.classList.remove('_sending');
-						} else {
-							responseBad.classList.add('_sending');
-							responseGood.classList.remove('_sending');
-							alert('Помилка');
-							alert(response.status);
-							form.reset();
-							form.classList.remove('_sending');
-						}
-					} else {
-						alert("Заповніть обов'язкові поля та перевірте email");
-						form.classList.remove('_sending');
-					}
-				}
-				function formValidate() {
-					let error = 0;
-					let formReq = document.querySelectorAll('._req');
-					for (let i = 0; i < formReq.length; i++) {
-						const input = formReq[i];
-						formRemoveError(input);
-
-						if (input.classList.contains('_email')) {
-							if (emailTest(input)) {
-								formAddError(input);
-								error++;
-							}
-						} else {
-							if (input.value === '') {
-								formAddError(input);
-								error++;
-							}
-						}
-					}
-					return error;
-				}
-				function formAddError(input) {
-					input.parentElement.classList.add('_error');
-					input.classList.add('_error');
-				}
-				function formRemoveError(input) {
-					input.parentElement.classList.remove('_error');
-					input.classList.remove('_error');
-				}
-				function emailTest(input) {
-					return !/^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,8})+$/.test(input.value);
-				}
-			});
-		</script>
-	</div>
+	<h1>Hi, please, input the data and send the form</h1>
+	<form class = "mainForm" bind:this = {form} on:submit|preventDefault={mainFormHandler}>
+		<input class = 'formInput' type="text" name = 'userName'placeholder="Enter your name" required >
+		<input class = 'formInput' type="email" name = 'userEmail'placeholder="Enter your email" required >
+		<textarea class = 'formText' name="userText"  cols="30" rows="10" placeholder="Input text" required />
+		<button class = "submitButton" type="submit" disabled = {disableButton}>
+			{#if showSpinner}
+				<Circle3 size="40" unit="px" duration="1s" />
+			{/if}
+			<h4>SEND</h4>
+		</button>
+		{#if showSuccess}
+			<p class = 'successMessage'>
+				Successfully sent!
+			</p>
+		{:else if showError}
+			<p class = 'errorMessage'>
+				Unfortunately, was not send. {textInError}
+			</p>
+		{/if}
+	</form>
 </section>
-
-<style>
-	section {
-		display: flex;
-		flex-direction: column;
-		justify-content: center;
-		align-items: center;
-		flex: 1;
-	}
-
-	h1 {
-		width: 100%;
-	}
-</style>
