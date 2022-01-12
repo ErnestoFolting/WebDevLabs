985043b37a54c9278a79aa2584467fd558d01d91
Danil <danilsmy.edu@gmail.com>
2022-01-12 13:43:46 +0200

    change error message

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -43,7 +43,7 @@
 			} else if (e.status === 429) {
 				textInError = 'You sent the form too many times';
 			} else {
-				textInError = 'Sorry. Error occured. You may have troubles with Internet.';
+				textInError = 'Sorry. Error occured.';
 			}
 		} finally {
 			showSpinner = false;
